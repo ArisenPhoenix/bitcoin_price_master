68395aef1e371648d10366b4ebd86ba39666e45f
Michael Marcure <marcure.phoenix.family@gmail.com>
2022-09-16 19:54:45 +0700

    Working on getting Auth0 to read envs.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const env = {
+  AUTH0_BASE_URL: "https://bitcoin-price-master.vercel.app/",
+};
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: env,
 };
 module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -8,14 +8,17 @@ import { NavigationContextProvider } from "../store/navigation-context";
 import BootStrapContainer from "../Components/UI/BootStrap/BootStrapContainer";
 
 function BitcoinGuesser({ Component, pageProps }) {
+  console.log("base_url: ", process.env.AUTH0_BASE_URL);
+  console.log("issuer base_url: ", process.env.AUTH0_ISSUER_BASE_URL);
+  console.log("next public base_url: ", process.env.NEXT_PUBLIC_AUTH0_BASE_URL);
   return (
     <SSRProvider>
       <ThemeProvider breakpoints={["xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}>
         <BootStrapContainer>
           <NavigationContextProvider>
             <UserProvider
-              loginUrl={process.env.AUTH0_BASE_URL + "/api/auth/login"}
-              profileUrl={process.env.AUTH0_BASE_URL + "/api/auth/me"}
+              loginUrl="https://bitcoin-price-master.vercel.app/api/auth/login"
+              profileUrl="https://bitcoin-price-master.vercel.app/api/auth/me"
             >
               <AUTH_CONTEXT_PROVIDER>
                 <Layout>
